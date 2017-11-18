c9e82759909f11b88a6d1e358f7c34bef7139226
Gyula Lakatos <laxika91@gmail.com>
2017-11-18 20:52:55 +0100

    Updated the supported version id.

--- fallen-sword-bot-api/src/main/java/com/fallensword/bot/api/RequestConfiguration.java
+++ fallen-sword-bot-api/src/main/java/com/fallensword/bot/api/RequestConfiguration.java
@@ -18,7 +18,7 @@ public class RequestConfiguration {
                 .baseUrl("https://www.fallensword.com/")
                 .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36")
                 .defaultHeader(HttpHeaders.HOST, "www.fallensword.com")
-                .defaultHeader(HttpHeaders.COOKIE, "fsId=152146; fsSessionKey=" + sessionId + ";")
+                .defaultHeader(HttpHeaders.COOKIE, "fsId=152420; fsSessionKey=" + sessionId + ";")
                 .defaultHeader(HttpHeaders.ACCEPT_LANGUAGE, "en-US,en;q=0.9")
                 .build();
     }
